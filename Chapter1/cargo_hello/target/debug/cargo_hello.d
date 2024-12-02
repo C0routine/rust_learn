613cdfaef7844d79e6d9c5b8f9c421a7cc3d9a84
Casan <whdrjsco@gmail.com>
2024-12-02 22:36:09 +0900

    fix: cargo rustc 사용방법 재학습

--- Chapter1/cargo_hello/target/debug/cargo_hello.d
+++ Chapter1/cargo_hello/target/debug/cargo_hello.d
@@ -1,1 +1,1 @@
-/Users/casan/Desktop/project/rust_learn/Chapter1/cargo_hello/target/debug/cargo_hello: /Users/casan/Desktop/project/rust_learn/Chapter1/cargo_hello/src/main.rs
+/Users/casan/project/rust_learn/Chapter1/cargo_hello/target/debug/cargo_hello: /Users/casan/project/rust_learn/Chapter1/cargo_hello/src/main.rs
--- Chapter1/cargo_hello/target/release/cargo_hello.d
+++ Chapter1/cargo_hello/target/release/cargo_hello.d
@@ -1,1 +1,1 @@
-/Users/casan/Desktop/project/rust_learn/Chapter1/cargo_hello/target/release/cargo_hello: /Users/casan/Desktop/project/rust_learn/Chapter1/cargo_hello/src/main.rs
+/Users/casan/project/rust_learn/Chapter1/cargo_hello/target/release/cargo_hello: /Users/casan/project/rust_learn/Chapter1/cargo_hello/src/main.rs
